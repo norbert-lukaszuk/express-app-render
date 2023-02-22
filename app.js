10fce354b2a0e3a26009087d8ce699dbb01963e9
norbert-lukaszuk <norbert.lukaszuk@gmail.com>
2023-02-22 11:36:40 +0100

    app: change res from file.json to json type

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const data = require("./data.json");
 const port = process.env.PORT || 3001;
 
-// app.get("/", (req, res) => res.type('html').send(html));
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "/index.html"));
 });
 app.get("/about", function (req, res) {
   res.sendFile(path.join(__dirname, "/about.html"));
 });
-app.get("/data", function (req, res) {
-  res.sendFile(path.join(__dirname, "/data.json"));
-});
+app.get("/data", (req, res) => res.type("json").send(data));
+// app.get("/data", function (req, res) {
+//   res.jsonp(path.join(__dirname, "/data.json"));
+// });
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
