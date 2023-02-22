8c7192a4b571cf71622f427b406b1a045e1bd0ec
norbert-lukaszuk <norbert.lukaszuk@gmail.com>
2023-02-22 11:54:11 +0100

    change res

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const data = require("./data.json");
+const data = import("./data.json");
 const port = process.env.PORT || 3001;
 
 app.get("/", function (req, res) {
@@ -10,7 +10,7 @@ app.get("/", function (req, res) {
 app.get("/about", function (req, res) {
   res.sendFile(path.join(__dirname, "/about.html"));
 });
-app.get("/data", (req, res) => res.type("json").send(data));
+app.get("/data", (req, res) => res.send(data));
 // app.get("/data", function (req, res) {
 //   res.jsonp(path.join(__dirname, "/data.json"));
 // });
